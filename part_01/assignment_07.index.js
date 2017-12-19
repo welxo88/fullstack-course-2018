9578eb748f0cbdcd1c7c16d2f4f5ee7aa013f2e1
welxo88 <welxo88@gmail.com>
2017-12-19 14:10:15 +0200

    Create assignment_07.index.js

---
+++ part_01/assignment_07.index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+  return (<h1>{props.kurssi}</h1>)
+}
+
+const Sisalto = (props) => {
+  return (
+    props.sisalto.map(el => {return <p>{el.osa} {el.tehtavia}</p>;})
+  )
+}
+
+const Yhteensa = (props) => {
+  let counter = 0;
+  props.sisalto.forEach(el => {counter += el.tehtavia})
+  return (<p>yhteensä {counter} tehtävää</p>)
+}
+
+const App = () => {
+  const kurssi = "Half Stack -sovelluskehitys"
+  const sisalto = [
+    {osa: 'Reactin perusteet', tehtavia: 10},
+    {osa: 'Tiedonvälitys propseilla', tehtavia: 7},
+    {osa: 'Komponenttien tila', tehtavia: 14},
+  ]
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto sisalto={sisalto} />
+      <Yhteensa sisalto={sisalto} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
